fe13cffc42facba72612a0d84aac78c39ead6419
Liang Chenghao <lch_office@foxmail.com>
2018-05-17 08:39:54 +0800

    update(the-basic): add more test instance.

--- the-basic/src/test/java/com/bonjourcs/algorithm/basic/BinarySearchTest.java
+++ the-basic/src/test/java/com/bonjourcs/algorithm/basic/BinarySearchTest.java
@@ -17,10 +17,12 @@ public class BinarySearchTest {
 
 		int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
 		Assert.assertEquals(4, BinarySearch.search(a, 5));
-		int[] b = {1, 2, 3, 4, 5, 6, 7};
-		Assert.assertEquals(3, BinarySearch.search(b, 4));
+		int[] b = {1, 2, 3, 4, 5, 6, 7, 8};
+		Assert.assertEquals(-1, BinarySearch.search(b, 10));
 		int[] c = {1, 2, 3, 4, 5, 6, 7};
-		Assert.assertEquals(-1, BinarySearch.search(c, 8));
+		Assert.assertEquals(3, BinarySearch.search(c, 4));
+		int[] d = {1, 2, 3, 4, 5, 6, 7};
+		Assert.assertEquals(-1, BinarySearch.search(d, 8));
 
 	}
 }
